4fb70c9ca5cba12309a0b0ca84660da7deb4ea72
AnishGeorgeJlabs <anish.george@jlabs.co>
2015-11-09 15:06:33 +0530

    initial commit

---
+++ __init__.py
@@ -0,0 +1,40 @@
+__author__ = 'jlabs-11'
+__package__ = 'cersei'
+
+import pymongo
+from django.http import HttpResponse
+from bson.json_util import dumps, loads
+
+# ------- Database Authentication and access ---------------- #
+dbclient = pymongo.MongoClient("45.55.232.5:27017")
+dbclient.tyrion.authenticate("cerseiApi", "baratheonQueen", mechanism='MONGODB-CR')
+
+db = dbclient.cersei
+
+def get_json(request):
+    return loads(request.body.decode())
+
+# ------- Json response format ------------------------------ #
+def jsonResponse(d):
+    return HttpResponse(dumps(d), content_type='application/json')
+
+def basic_failure(reason=None):
+    return base_response(success=False, ekey="reason", reason=reason)
+
+def basic_error(reason=None):
+    return base_response(success=False, ekey="error", reason=str(reason))
+
+def basic_success(data=None):
+    return base_response(success=True, data=data)
+
+def base_response(success=False, data=None, ekey="reason", reason=None):
+    res = {"success": success}
+
+    if data is not None:
+        res['data'] = data
+
+    if reason is not None:
+        res[ekey] = reason
+    return jsonResponse(res)
+
+# -------------------------------------------------------------- #
---
+++ urls.py
@@ -0,0 +1,46 @@
+from django.conf.urls import url
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+
+#from . import api, security, merchant_api
+
+
+@csrf_exempt
+def test(request):
+    if request.method == "GET":
+        extra = {
+            "method": "GET",
+            "requestData": request.GET
+        }
+    else:
+        extra = {
+            "method": "POST",
+            "requestData": request.body
+        }
+    return JsonResponse({
+        "result": True,
+        "Message": "Test api, ECHO",
+        "extra": extra
+    })
+
+urlpatterns = [
+    url(r'^$', test)
+    """
+
+    # --------- Customer app urls --------- #
+    url(r'^menu$', api.get_menu),
+    url(r'^menu/check_version$', api.check_menu_version),
+    url(r'order$', api.place_order),
+
+    # ---------- Merchant app urls -------- #
+    url(r'^login$', security.login),
+    url(r'^change_pass$', security.change_password),
+
+    url(r'^order_list$', security.auth(merchant_api.get_order_list)),
+    url(r'^order_data$', security.auth(merchant_api.get_complete_order)),
+    url(r'^order_data/update_status$', security.auth(merchant_api.update_status))
+    """
+
+]
+
+
