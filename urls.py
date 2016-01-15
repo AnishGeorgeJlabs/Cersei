8590810a37a0c7160c4c09416bb52d563e3b0a2a
AnishGeorgeJlabs <anish.george@jlabs.co>
2016-01-15 12:26:07 +0530

    A lil change here, a lil there

--- urls.py
+++ urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from . import mock, security
-from .vendor import order, retailer
+from .vendor import order, account
 from .consumer import show_offers, search_location,rewards
 
 @csrf_exempt
@@ -25,19 +25,24 @@ def test(request):
 
 urlpatterns = [
     url(r'^$', test),
+
+    # ------------ Auth URL's ---------------------------
+    url(r'^auth/login$', security.login),
+    url(r'^auth/change_pass$', security.change_password),
+
+    # ------------ Vendor URL's -------------------------
     url(r'^order/list$', security.auth(order.order_list)),
     url(r'^order/details$', mock.details),
     url(r'^order/update', security.auth(order.update_order)),
     url(r'^order/scan$', security.auth(order.inner_scan)),
     url(r'^order/scan/new$', security.auth(order.new_scan)),
-    url(r'^retailer/account', security.auth(retailer.vendor_account)),
+    url(r'^retailer/account', security.auth(account.vendor_account)),
 
+    # ------------ Consumer URL's -----------------------
     url(r'^location',  search_location.search_query),
     url(r'^show_offers',  show_offers.show_offers),
+    url(r'^reward', rewards.activeRewards),
 
-    url(r'^auth/login$', security.login),
-    url(r'^auth/change_pass$', security.change_password),
-	url(r'^reward', rewards.activeRewards)
 ]
 
 
