a9145729f7486d08a74056ef5cd377d55bbdcd02
Gopal <gopal@jlabs.co>
2016-03-20 07:03:23 -0700

    Minor changes with FE APIs

--- fe/fe_offers.py
+++ fe/fe_offers.py
@@ -12,22 +12,56 @@ failure = dumps({"success": 0})
 @csrf_exempt
 def list_item(request):
 	try:
+		fe_id = request.GET['fe_id']
+		
+	except:
+		return basic_failure("Inavalid key parameters")
+	try:
+		data=db.fe_db
+		fe = data.find_one({"fe_id":int(fe_id)} , {"_id":False})
+		if not fe:
+			return basic_failure("Not found")
+		company_id = fe['company_id']
 		data=db.items_alt
-		result=data.find(projection={"_id":False})
+		result=data.find({"company_id":{"$in":company_id}} , {"_id":False})
 		return basic_success(result)
 	except:
 		return basic_failure()
 @csrf_exempt
 def list_vendor(request):
 	try:
+		fe_id = request.GET['fe_id']
+		
+	except:
+		return basic_failure("Inavalid key parameters")
+	try:
+		data=db.fe_db
+		fe = data.find_one({"fe_id":int(fe_id)} , {"_id":False})
+		if not fe:
+			return basic_failure("Not found")
+		mystores = fe['my_store']	
 		data=db.vendors_alt
 		result=data.find(projection={"_id":False , "vendor_id":1 , "name":1 , "address.address":1})
-		return basic_success(result)
+		return basic_success({"stores":result , "mystore":mystores})
 	except:
 		return basic_failure()
 @csrf_exempt
 def list_offers(request):
 	try:
+		try:
+			fe_id = request.GET['fe_id']
+		except:
+			return basic_failure("Inavalid key parameters")
+		data=db.fe_db
+		fe = data.find_one({"fe_id":int(fe_id)} , {"_id":False})
+		if not fe:
+			return basic_failure("Not found")
+		company_id = fe['company_id']
+		data=db.items_alt
+		result=data.find({"company_id":{"$in":company_id}} , {"_id":False})
+		items_id=list()
+		for res in result:
+			items_id.append(res['item_id'])
 		data=db.codes_alt
 		result=list(data.aggregate( [
 			{
@@ -55,7 +89,9 @@ def list_offers(request):
 		aa = list(data.aggregate([
 			{
 			   "$match":{
-					"expired":False
+					"expired":False , 
+					"item_id":{
+						"$in":items_id}
 				}
 			},
 			{
@@ -91,11 +127,12 @@ def create_offers(request):
 		total_offer=dt['count']
 		dataa=list()
 		countt=0
+		all_offers_id=list()
 		for new in new_offer:
-			DOM = (new['dom']).split("-")
+			DOM = (new['dom']).split("/")
 			day = calendar.monthrange(int(str(DOM[0])) , int(str(DOM[1])))[1]
 			DM=datetime.strptime(new['dom']+"-"+str(day) , "%Y-%m-%d")
-			DOE = (new['doe']).split("-")
+			DOE = (new['doe']).split("/")
 			day = calendar.monthrange(int(str(DOE[0])) , int(str(DOE[1])))[1]
 			DE=datetime.strptime(new['doe']+"-"+str(day) , "%Y-%m-%d")
 			item_id=new['item_id']
@@ -114,6 +151,7 @@ def create_offers(request):
 					code['offer_id']=max1
 					codes.append(code.copy())
 			result=data.insert({"offer_id":max1 , "dom":DM , "expiry":DE , "points":int(points) , "fe_id":[ fe_id] , "item_id":item_id , "vendor_id":vendor_list , "expired":False})
+			all_offers_id.append(max1)
 			countt+=1
 		for old in old_offer:
 			vid=old['vendors']
@@ -129,12 +167,15 @@ def create_offers(request):
 					code['offer_id']=offer_id
 					codes.append(code.copy())
 			result = data.find_one_and_update({"offer_id":offer_id} ,{	"$addToSet":{"fe_id":fe_id,"vendor_id":{"$each":vendor_list}	}})
+			all_offers_id.append(offer_id)
 			countt+=1
-			data=db.codes_alt
-			result = data.insert_many(codes)
+		data=db.codes_alt
+		result = data.insert_many(codes)
+		data=db.fe_db
+		result = data.find_one_and_update({"fe_id":fe_id} , {"$addToSet":{"$each":all_offers_id}})
 		return basic_success(countt)
-	except Exception as e:
-		return basic_failure(e)
+	except:
+		return basic_failure()
 	
 		
 	
