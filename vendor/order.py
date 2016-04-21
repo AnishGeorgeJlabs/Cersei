bc3e796fc49cd5a023a2d704bb5342d7de5ec28b
Gopal <gopal@jlabs.co>
2016-04-21 15:06:57 +0530

    changes in retailer scan process

--- vendor/order.py
+++ vendor/order.py
@@ -126,26 +126,49 @@ def update_order(opts, vendor_id, method):
 
 
 def inner_scan(opts, vendor_id, method):
-    code = opts.get("code")
-    if not code:
-        return basic_error("No code given")
-    data = db.codes.find_one({"code": code, "used": False}, {"barcode": True, "pts": True, "_id": False})
-    if not data:
-        return basic_failure("Already used")
-    else:
-        return basic_success(data)
-
+	code = opts.get("code")
+	order_id = opts.get("order_id")
+	if not code or not order_id:
+		return basic_error("Invalid parameters")
+	code_data = db.codes_alt.find_one({"code": code}, {"_id": False })
+	if not code_data:
+		return basic_failure("Code not found")
+	if code_data['used']:
+		return basic_failure("Already used")
+	qrcodes=db.orders.find_one({"order_id":order_id} , {"_id":False , "order":True})
+	barcodes=list()
+	for qrcode in qrcodes['order']:
+		barcodes.append(qrcode['barcode'])
+	offer = db.offers_alt.find_one({"offer_id":int(code_data['offer_id'])})
+	item = db.items_alt.find_one({"item_id": offer['item_id']}, {"_id": False, "name": 1, "barcode": 1, "img":1 , "weight":1})
+	if item['barcode'] in barcodes:
+		return basic_success({
+			"code": code,
+			"pts": offer['points'],
+			"item": item,
+			"codematch":True
+		})
+	else:
+		return basic_success({
+			"code": code,
+			"pts": offer['points'],
+			"item": item,
+			"codematch":False
+		})
 
 def new_scan(opts, vendor_id, method):
-    code = opts.get("code")
-    if not code:
-        return basic_error("No code given")
-    code_data = db.codes_alt.find_one({"code": code, "used": False}, {"_id": False })
-    if not code_data:
-        return basic_failure("Already used")
-    item = db.items_alt.find_one({"barcode": code_data['barcode']}, {"_id": False, "name": 1, "barcode": 1})
-    return basic_success({
-        "code": code,
-        "pts": code_data['pts'],
-        "item": item
-    })
+	code = opts.get("code")
+	if not code:
+		return basic_error("No code given")
+	data = db.codes_alt.find_one({"code": code, "used": False}, {"code": True, "offer_id": True, "_id": False})
+	if not data:
+		return basic_failure("Already Used")
+	else:
+		offer = db.offers_alt.find_one({"offer_id":int(data['offer_id'])})
+		item = db.items_alt.find_one({"item_id": offer['item_id']}, {"_id": False, "name": 1, "barcode": 1, "img":1 , "weight":1})
+		return basic_success({
+			"code": code,
+			"pts": offer['points'],
+			"item": item
+			
+		})
